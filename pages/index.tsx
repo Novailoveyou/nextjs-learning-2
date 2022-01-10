9a86139549ac7c63077f10458800d0926a2fe024
Novailoveyou <novailoveyoux@gmail.com>
2022-01-10 15:37:13 +0300

    imports order adjustments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import { getSortedPostsData } from '../lib/posts'
+import Layout, { siteTitle } from '../components/layout'
+import Date from '../components/date'
 
 export default function Home({
   allPostsData
